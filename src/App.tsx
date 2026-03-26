5342f16f914c35098755c797da1ca67c691b72e5
Daniel <danielf.4691@gmail.com>
2026-03-26 18:34:01 -0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Movie } from './components/Types/Movie';
+
+function getPreparedMovies(movies: Movie[], query: string) {
+  let preparedMovies = [...movies];
+  const trimmedQuery = query.trim();
+
+  if (trimmedQuery) {
+    preparedMovies = preparedMovies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(trimmedQuery.toLowerCase()) ||
+        movie.description?.toLowerCase().includes(trimmedQuery.toLowerCase()),
+    );
+  }
+
+  return preparedMovies;
+}
 
 export const App: React.FC = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovies(moviesFromServer, query);
+
   return (
     <div className="page">
       <div className="page-content">
@@ -16,6 +36,8 @@ export const App: React.FC = () => {
 
             <div className="control">
               <input
+                value={query}
+                onChange={event => setQuery(event.target.value)}
                 type="text"
                 id="search-query"
                 className="input"
@@ -25,7 +47,7 @@ export const App: React.FC = () => {
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
